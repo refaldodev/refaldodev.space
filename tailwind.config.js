20262c9e63bf6385e48f800d62ab9315ffd82511
Mohammad Refaldo <m.refaldorachman@gmail.com>
2022-01-08 16:21:08 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  important: true,
+
+  theme: {
+    extend: {},
+    fontFamily: {
+      "theme-f1": ['"Oswald"', "sans-serif"],
+      "theme-f2": ['"Lora"', "serif"],
+      "theme-f3": ['"Bebas Kai"', "sans-serif"],
+      "theme-f4": ['"Open Sans"', "sans-serif"],
+    },
+  },
+  plugins: [],
+};
