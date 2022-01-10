9ce8b4c8020cd1579325c50793aab8f6defa2345
Mohammad Refaldo <m.refaldorachman@gmail.com>
2022-01-10 10:07:55 +0700

    add seo and work

--- src/App.js
+++ src/App.js
@@ -192,17 +192,48 @@ function App() {
         <div className="main mt-10 mb-20">
           <div className="title">
             <h1 className="font-extrabold	 text-lg tracking-wide text-white  ">
-              Portfolio
+              Work
             </h1>
           </div>
           <a
             className="App-link"
-            href={Cv}
-            target="_blank"
+            href="mailto: m.refaldorachman@gmail.com"
             rel="noopener noreferrer"
             download="cv refaldo"
           >
-            <div className="links bg-teal-500			p-4 max-w-lg mx-auto mt-3 flex items-center align-middle text-white rounded-full hover:bg-teal-600	">
+            <div className="links bg-teal-500	p-4 max-w-lg mx-auto mt-3 flex items-center align-middle text-white rounded-full hover:bg-teal-600	">
+              <div className="svg-icon w-1/6 ">
+                <svg
+                  aria-hidden="true"
+                  focusable="false"
+                  data-prefix="fas"
+                  data-icon="download"
+                  className="svg-inline--fa fa-download fa-w-16"
+                  role="img"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512"
+                  height="24px"
+                  width="24px"
+                >
+                  <path
+                    fill="white"
+                    d="M216 0h80c13.3 0 24 10.7 24 24v168h87.7c17.8 0 26.7 21.5 14.1 34.1L269.7 378.3c-7.5 7.5-19.8 7.5-27.3 0L90.1 226.1c-12.6-12.6-3.7-34.1 14.1-34.1H192V24c0-13.3 10.7-24 24-24zm296 376v112c0 13.3-10.7 24-24 24H24c-13.3 0-24-10.7-24-24V376c0-13.3 10.7-24 24-24h146.7l49 49c20.1 20.1 52.5 20.1 72.6 0l49-49H488c13.3 0 24 10.7 24 24zm-124 88c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20zm64 0c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20z"
+                  ></path>
+                </svg>
+              </div>
+              <div className="text grow ">
+                <p className="font-medium	">Email Me</p>
+              </div>
+              <div className="space  w-1/6"></div>
+            </div>
+          </a>
+          <a
+            className="App-link"
+            href={Cv}
+            rel="noopener noreferrer"
+            download="cv refaldo"
+          >
+            <div className="links bg-emerald-500	p-4 max-w-lg mx-auto mt-3 flex items-center align-middle text-white rounded-full hover:bg-emerald-600	">
               <div className="svg-icon w-1/6 ">
                 <svg
                   aria-hidden="true"
